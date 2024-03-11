c024c750a03a6b304e5aaca33f00292a5168fd5d
Lydia <guipopo5@gmail.com>
2024-03-11 17:02:46 +0800

    Initial Commit

---
+++ Project16-Capital Cities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project16-Capital Cities
+//
+//  Created by Lydia Lu on 2024/3/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
